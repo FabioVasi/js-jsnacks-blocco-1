04e34c7aa4e5fd4e819a3668a77b6734140234f8
FabioVasi <vasifabio96@gmail.com>
2023-08-29 17:38:20 +0200

    end ex

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -5,52 +5,24 @@ Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 */
 
-const numbers = [''];
+// array vuoto
+const numbers = [];
+const userNumbers = [];
 
-const number_1 = Number(prompt('Number 1, type here.'));
-const number_2 = Number(prompt('Number 2, type here.'));
-const number_3 = Number(prompt('Number 3, type here.'));
-const number_4 = Number(prompt('Number 4, type here.'));
-const number_5 = Number(prompt('Number 5, type here.'));
-const number_6 = Number(prompt('Number 6, type here.'));
-
-console.log(number_1, number_2, number_3, number_4, number_5, number_6);
-
-if(number_1 % 2 !== 0) {
-
-    numbers = numbers.push(number_1);
-
-    console.log(numbers);
-
-} else if (number_2 % 2 !== 0) {
-
-    numbers = numbers.push(number_2);
-
-    console.log(numbers);
-
-} else if (number_3 % 2 !== 0) {
+// chiedo all'utente di inserire 6 numeri casuali
+for (let i = 0; i < 6; i++) {
     
-    numbers = numbers.push(number_3);
+    userNumbers.push(Number(prompt(`Tasks ${i}, type here a random number.`)));
+    // loggo in console i numeri inseriti dall'utente
+    console.log(userNumbers);
 
-    console.log(numbers);
+    // se il numero inserito è dispari lo inserisco nell'array e vado al prossimo numero fino a controllarli tutti
+    // se il numero inserito è pari lo ignoro e passo al prossimo numero fino a controllarli tutti
+    if (userNumbers[i] % 2 !== 0) {
 
-} else if (number_4 % 2 !== 0) {
+        numbers.push(userNumbers[i]);
 
-    numbers = numbers.push(number_4);
-
-    console.log(numbers);
-
-} else if (number_5 % 2 !== 0) {
-
-    numbers = numbers.push(number_5);
-
-    console.log(numbers);
-
-} else if (number_6 % 2 !== 0) {
-
-    numbers = numbers.push(number_6);
-
-    console.log(numbers);
-
+    }
+    
 }
-
+console.log(numbers);
