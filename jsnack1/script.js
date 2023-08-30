9f1f268e00d7fe77066f8ccc45f9057f2e680920
FabioVasi <vasifabio96@gmail.com>
2023-08-30 10:51:47 +0200

    add cycle while

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,7 +2,7 @@
 Snack 1:
 L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
-*/
+
 
 const userFirstNumber = Number(prompt('Type a number here.'));
 
@@ -22,4 +22,39 @@ if(userFirstNumber > userSecondNumber) {
 
     console.log('I numeri sono uguali');
 
+}
+*/
+
+/*
+Da fare con WHILE tutti I prossimi snacks
+Snack 1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+const numberOne = Number(prompt('Type a number here.'));
+
+const numberTwo = Number(prompt('Type a second number here.'));
+
+console.log('first number ', numberOne, 'second number ', numberTwo);
+
+let i = 0;
+
+while (i < 2) {
+
+    if(numberOne > numberTwo) {
+
+        console.log('Il primo numero è maggiore del secondo');
+    
+    } else if(numberTwo > numberOne) {
+    
+        console.log('Il secondo numero è maggiore del primo');
+    
+    } else {
+    
+        console.log('I numeri sono uguali');
+    
+    }
+
+    i++ 
 }
