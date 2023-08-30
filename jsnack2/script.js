6d0fffbc370e10203359321846056e33ca1e73b9
FabioVasi <vasifabio96@gmail.com>
2023-08-30 11:55:24 +0200

    snack 2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,7 +2,6 @@
 Snack 2:
 L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
-*/
 
 const firstWord = prompt('Type a word here');
 
@@ -22,4 +21,42 @@ if(firstWord.length > secondWord.length){
 
     console.log('Le parole hanno la stessa lunghezza');
     
+}
+*/
+
+
+/*
+(con while) Snack 2:
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+let i = 0;
+
+const wordList = []
+
+while (i < 2) {
+
+    const userword = prompt('type a word here');
+
+    console.log(userword);
+
+    wordList.push(userword);
+
+    i++
+
+}
+
+if(wordList[1] > wordList[0]) {
+
+    console.log('La prima parola è più lunga');
+
+} else if(wordList[0] > wordList[1]) {
+
+    console.log('La seconda parola è più lunga');
+
+} else {
+
+    console.log('Le parole hanno la stessa lunghezza');
+
 }
