35c8a5441e6d14a22c0f3935fe4d778fec31bbd8
FabioVasi <vasifabio96@gmail.com>
2023-08-30 12:32:52 +0200

    ad

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,7 +2,7 @@
 Snack 3:
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
-*/
+
 
 const number_1 = Number(prompt('Number 1, type here.'));
 const number_2 = Number(prompt('Number 2, type here.'));
@@ -20,3 +20,16 @@ console.log(number_1, number_2, number_3, number_4, number_5, number_6, number_7
 let totale = Number(number_1 + number_2 + number_3 + number_4 + number_5 + number_6 + number_7 + number_8 + number_9 + number_10);
 
 console.log(totale);
+*/
+
+const numbersList = [Number(prompt('Number 1, type here.')), Number(prompt('Number 2, type here.')), Number(prompt('Number 3, type here.')), Number(prompt('Number 4, type here.')), Number(prompt('Number 5, type here.')), Number(prompt('Number 6, type here.')), Number(prompt('Number 7, type here.')), Number(prompt('Number 8, type here.')), Number(prompt('Number 9, type here.')), Number(prompt('Number 10, type here.'))]
+
+console.log(numbersList);
+
+for (let i = 0; i < 10; i++) {
+
+    const userNumbers = numbersList[i];
+    
+    console.log(userNumbers); 
+
+}
